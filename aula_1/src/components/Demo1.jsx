cfa1bd882edd0b302b33814156635617ce5e3030
Gucrima <gusssoares09@gmail.com>
2025-02-06 17:23:57 -0300

    revisão barba do rafa

--- aula_1/src/components/Demo1.jsx
+++ aula_1/src/components/Demo1.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 import './Demo1.css'
 
 function Demo1() {
-    function alertarUsuario(){
+    const [estado, setEstado] = useState(0); // define um estado inicial de 0
+    function aumentaNumero(){ // função que aumenta o estado em 1 a cada clique
+        setEstado(estado + 1)
+    }
+
+    let cornometro = 0; // variável que armazena o nnúmero de cliques
+
+    
+    function contadorDoRafa(){ // função que aumenta o cornometro em 1 a cada clique
+        cornometro = cornometro + 1;
+        console.log(cornometro)
+    }
+
+    function alertarUsuario(){ // função que alerta o usuário
         alert('Você é muito curioso 🧐')
     }
 
@@ -11,6 +25,17 @@ function Demo1() {
       Demo Do Rafael
       <button onClick={alertarUsuario}>Clique aqui</button>
       <button onClick={() => {alert('executei um arrow Function')}}>Arrow Function</button>
+
+    <div>
+      cornometro: {cornometro}
+      <button onClick={contadorDoRafa}>+</button>
+    </div>
+    <div>
+    {/* exibe o estado atualizados */}
+        estado: {estado} 
+    {/* aciona a função aumentaNumero ao clicar no botão  */}
+        <button onClick={aumentaNumero}>+</button>
+    </div>
     </div>
   )
 }
