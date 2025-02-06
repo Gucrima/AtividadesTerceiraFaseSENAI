641af1df23e899a0f0a50a9cedaaa3845f1c349d
Gucrima <gusssoares09@gmail.com>
2025-02-06 16:57:21 -0300

    mais e mais e mais revisão do rafa

--- aula_1/src/components/Demo1.jsx
+++ aula_1/src/components/Demo1.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import './Demo1.css'
 
 function Demo1() {
+    function alertarUsuario(){
+        alert('Você é muito curioso 🧐')
+    }
+
   return (
     <div className='conteiner-demo1'>
       Demo Do Rafael
+      <button onClick={alertarUsuario}>Clique aqui</button>
+      <button onClick={() => {alert('executei um arrow Function')}}>Arrow Function</button>
     </div>
   )
 }
