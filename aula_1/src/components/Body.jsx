608426cef6cae9d6a9e9a44e7ae29ec6b3db072f
Gucrima <gusssoares09@gmail.com>
2025-02-10 16:46:13 -0300

    Caiu a luz do senai na aula do rafa

--- aula_1/src/components/Body.jsx
+++ aula_1/src/components/Body.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './Body.css'
 import Demo1 from './Demo1'
 import Avisos from './Avisos'
+import Demo2 from './Demo2'
 
 function Body() {
   return (
     <div className='conteiner-body'>
-      <p>algo Legal</p>
       <Demo1 />
+      <Demo2 />
       <Avisos />  
     </div>
   )
---
+++ aula_1/src/components/Demo2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AiFillGithub } from "react-icons/ai";
+import './Demo2.css'
+
+function Demo2() {
+  return (
+    <div className='conteiner-demo2'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmgBGQWye0ctBcXUCgvxXWC-WcXP1XQ_zgow&s" alt="" />
+      <img src="./Images/Captura de tela 2025-02-10 161115.png" alt="" className='img-demo'/>
+      <AiFillGithub className='gato-git'/>
+    </div>
+  )
+}
+
+export default Demo2
